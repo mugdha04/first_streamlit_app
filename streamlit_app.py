cb874ebf00f635b805aa0fe14b7bef048b6fa79d
mugdha04 <54259516+mugdha04@users.noreply.github.com>
2023-09-25 23:24:34 +0530

    Update streamlit_app.py
    Re-Organized Version

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 streamlit.title('My New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -30,6 +31,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # shows output on the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
