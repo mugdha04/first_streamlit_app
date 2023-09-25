2f66827a5028a8c72bde8ecb6add2886508de8e5
mugdha04 <54259516+mugdha04@users.noreply.github.com>
2023-09-25 22:30:44 +0530

    Update streamlit_app.py
    Check to Confirm the Snowflake Connector Package is Add Successfully - imported snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import snowflake.connector
 streamlit.title('My New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
